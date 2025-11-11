23775723d3809ca09246871b64558405945f8880
Nicholas s Bacakos <nickbackospsn@gmail.com>
2025-11-10 19:58:53 -0600

    feat: Add backend API endpoints for movie carousels
    - Add 9 movie category endpoints (/api/movies/*)
    - Integrate TMDb API for real movie data (popular, now-playing, discover by genre)
    - Implement 5-minute caching for movie data
    - Add movie details endpoint for full movie information
    - Update Movie model to include posterUrl

--- server/src/models/Movie.js
+++ server/src/models/Movie.js
@@ -5,6 +5,10 @@ class Movie {
     this.overview = data.overview ?? '';
     this.posterPath = data.poster_path ?? '';
     this.releaseDate = data.release_date ?? '';
+    // Generate full poster URL if posterPath exists
+    this.posterUrl = data.poster_path 
+      ? `https://image.tmdb.org/t/p/w500${data.poster_path}`
+      : 'https://placehold.co/300x450/222/FFF?text=No+Poster';
   }
 }
 
